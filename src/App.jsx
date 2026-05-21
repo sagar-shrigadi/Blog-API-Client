98827ed1793de191914a33b4e785a88c3b1351e7
sagar-shrigadi <181492974+sagar-shrigadi@users.noreply.github.com>
2026-05-21 12:24:49 +0530

    fix: header and footer imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router";
 import "./App.css";
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/Header";
 import { useEffect, useState } from "react";
 import { getUserById } from "./service/user/getUser";
+import { Header } from "./components/Header/Header";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   const [user, setUser] = useState(null);
